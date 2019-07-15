404a5d973b0e5ac5a22be8b7db7e79abd3bcae97
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-15 16:07:00 -0700

    Harvester now stores targets to give energy to in memory

--- role.harvester.js
+++ role.harvester.js
@@ -10,29 +10,34 @@ module.exports = {
             // Sends creep to gather enerygy
             creep.getEnergy(true, true, true);
         } else {
-            // Make sure spawner and all extensions are filled before considering Towers
-            const extension = creep.pos.findClosestByPath(FIND_MY_STRUCTURES,
-                {filter: (s) => (s.structureType == STRUCTURE_EXTENSION ||
-                                s.structureType == STRUCTURE_SPAWN) &&
-                                s.energy < s.energyCapacity});
-            if (extension != undefined) {
-                if (creep.transfer(extension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.signaledMove(extension);
+            // Selecting the target to fill up
+            if (creep.memory.energy == undefined) {
+                // Make sure spawner and all extensions are filled before considering Towers
+                const extension = creep.pos.findClosestByPath(FIND_MY_STRUCTURES,
+                    {filter: (s) => (s.structureType == STRUCTURE_EXTENSION ||
+                                    s.structureType == STRUCTURE_SPAWN) &&
+                                    s.energy < s.energyCapacity});
+                if (extension != undefined)
+                    creep.memory.energy = extension.id;
+
+                // If all extensions/spawns are filled then find other structures
+                else {
+                    const structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES,
+                        {filter: (s) => s.structureType != STRUCTURE_LINK &&
+                                        s.energy < s.energyCapacity});
+                    if (structure != undefined)
+                        creep.memory.energy = structure.id;
+                    else
+                        roleBuilder.run(creep);
                 }
-                return;
             }
 
-            // Find all Spawners and Towers in the room
-            let target = creep.pos.findClosestByPath(FIND_STRUCTURES,
-                {filter: (s) => s.structureType != STRUCTURE_LINK &&
-                                s.energy < s.energyCapacity});
-
-            if (target != undefined) {
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.signaledMove(target);
-                }
+            // Transfer energy to the target specified in memory, afterwards get new target
+            const target = Game.getObjectById(creep.memory.energy);
+            if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.signaledMove(target);
             } else {
-                roleBuilder.run(creep);
+                delete creep.memory.energy;
             }
         }
     }
