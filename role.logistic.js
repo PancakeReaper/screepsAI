34766adb075176b11f423a1f30f32519f78cacaa
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-15 16:06:31 -0700

    Updated logistic to do harvester tasks during downtime

--- role.logistic.js
+++ role.logistic.js
@@ -1,9 +1,21 @@
+const roleHarvester = require("role.harvester");
 
 module.exports = {
 
     run: function(creep) {
         // Updates creep's memory variables
         creep.update();
+
+        const energy = creep.room.find(FIND_STRUCTURES,
+            {filter: (s) => (s.structureType == STRUCTURE_EXTENSION ||
+                            s.structureType == STRUCTURE_SPAWN ||
+                            s.structureType == STRUCTURE_TOWER) &&
+                            s.energy < s.energyCapacity});
+                            
+        if (energy.length > 0) {
+            roleHarvester.run(creep);
+            return;
+        }
 
         if (!creep.memory.working) {
             // Only get energy from containers that are close to being full
@@ -18,7 +30,11 @@ module.exports = {
             }
         } else {
             // Store energy in the room's storage
-            const target = creep.room.storage;
+//            const target = creep.room.storage;
+            const target = creep.pos.findClosestByPath(FIND_STRUCTURES,
+                {filter: (s) => (s.structureType == STRUCTURE_TOWER ||
+                                s.structureType == STRUCTURE_EXTENSION) &&
+                                s.energy < s.energyCapacity});
 
             if (target != undefined) {
                 if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
