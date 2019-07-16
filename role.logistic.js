4781a0d655fc9da8a38f75d4840fd56ced1c2ad8
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-16 13:37:07 -0700

    Refactored to storing targets in memory & no longer fills up towers

--- role.logistic.js
+++ role.logistic.js
@@ -8,37 +8,49 @@ module.exports = {
 
         const energy = creep.room.find(FIND_STRUCTURES,
             {filter: (s) => (s.structureType == STRUCTURE_EXTENSION ||
-                            s.structureType == STRUCTURE_SPAWN ||
-                            s.structureType == STRUCTURE_TOWER) &&
+                            s.structureType == STRUCTURE_SPAWN) &&
                             s.energy < s.energyCapacity});
-                            
+
         if (energy.length > 0) {
             roleHarvester.run(creep);
             return;
         }
 
         if (!creep.memory.working) {
-            // Only get energy from containers that are close to being full
-            const container = creep.pos.findClosestByPath(FIND_STRUCTURES,
-                {filter: (s) => s.structureType == STRUCTURE_CONTAINER &&
-                            s.store[RESOURCE_ENERGY] / s.storeCapacity > 0.75});
+            if (creep.memory.container == undefined) {
+                // Only get energy from containers that are close to being full
+                const container = creep.pos.findClosestByPath(FIND_STRUCTURES,
+                    {filter: (s) => s.structureType == STRUCTURE_CONTAINER &&
+                                    s.store[RESOURCE_ENERGY] / s.storeCapacity > 0.75});
+                if (container != undefined)
+                    creep.memory.container = container.id;
+            }
 
-            if (container != undefined) {
+            if (creep.memory.container != undefined) {
+                const container = Game.getObjectById(creep.memory.container);
                 if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.signaledMove(container);
+                } else {
+                    delete creep.memory.container;
                 }
             }
         } else {
-            // Store energy in the room's storage
-//            const target = creep.room.storage;
-            const target = creep.pos.findClosestByPath(FIND_STRUCTURES,
-                {filter: (s) => (s.structureType == STRUCTURE_TOWER ||
-                                s.structureType == STRUCTURE_EXTENSION) &&
-                                s.energy < s.energyCapacity});
+            if (creep.memory.storage == undefined) {
+                // Store energy in the room's storage
+                const storage = creep.pos.findClosestByPath(FIND_STRUCTURES,
+                    {filter: (s) => (s.structureType == STRUCTURE_EXTENSION &&
+                                    s.energy < s.energyCapacity) ||
+                                    s.structureType == STRUCTURE_STORAGE});
+                if (storage != undefined)
+                    creep.memory.storage = storage.id;
+            }
 
-            if (target != undefined) {
+            if (creep.memory.storage != undefined) {
+                const target = Game.getObjectById(creep.memory.storage);
                 if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.signaledMove(target);
+                } else {
+                    delete creep.memory.storage;
                 }
             }
         }
