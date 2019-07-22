7941ed9fb2a9f352c23959cd020289e7d35adde4
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-22 13:33:36 -0700

    Quarry now stores container in memory, instead of finding it every tick

--- role.quarry.js
+++ role.quarry.js
@@ -3,26 +3,29 @@ module.exports = {
 
     run: function(creep) {
 
-        // Find a container has a source adjacent, have no other creep ontop, and is not full
-        const container = creep.pos.findClosestByPath(FIND_STRUCTURES,
-            {filter: (s) => s.structureType == STRUCTURE_CONTAINER &&
-                            (creep.pos.isEqualTo(s) || !s.pos.lookFor(LOOK_CREEPS).length) &&
-                            s.pos.findInRange(FIND_SOURCES, 2).length > 0 &&
-                            _.sum(s.store) < s.storeCapacity});
-        const energy_source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        if (creep.memory.container == undefined) {
+            // Find a container has a source adjacent, have no other creep ontop, and is not full
+            const container = creep.pos.findClosestByPath(FIND_STRUCTURES,
+                {filter: (s) => s.structureType == STRUCTURE_CONTAINER &&
+                                (creep.pos.isEqualTo(s) || !s.pos.lookFor(LOOK_CREEPS).length) &&
+                                s.pos.findInRange(FIND_SOURCES, 2).length > 0 &&
+                                _.sum(s.store) < s.storeCapacity});
+            if (container != undefined)
+                creep.memory.container = container.id;
+            else
+                return;
+        }
+        const container = Game.getObjectById(creep.memory.container);
 
-        if (container == undefined)
-            creep.say("No available container found");
-        else if (!creep.pos.isEqualTo(container.pos))
+        if (!creep.pos.isEqualTo(container.pos))
             creep.signaledMove(container);
-        else if (energy_source != undefined) {
+        else {
+            const energy_source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
             if (creep.harvest(energy_source) == ERR_NOT_IN_RANGE) {
                 creep.say("Out of range");
             } else {
                 creep.room.visual.text(_.sum(container.store) + "/" + container.storeCapacity, creep.pos);
             }
-        } else {
-            creep.say("source out");
         }
     }
 };
