c057af44dec9013860b23b4c4abde2d417fd6bf5
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-24 14:09:14 -0700

    Re-ordered tower target priority

--- towerAI.js
+++ towerAI.js
@@ -2,16 +2,6 @@ var cb = require('controlBoard');
 
 module.exports = {
     run: function(tower) {
-        // Repair walls and ramparts until health === cb.repairWallsTo
-        let targets = tower.room.find(FIND_STRUCTURES,
-            {filter: (s) => (s.structureType == STRUCTURE_WALL ||
-                            s.structureType == STRUCTURE_RAMPART) &&
-                            s.hits < cb.repairWallsTo});
-        targets = _.sortBy(targets, (t) => t.hits);
-        if (targets.length) {
-            tower.repair(targets[0]);
-            return;
-        }
 
         // Heal a creep if less than 50% health
         let creep = tower.room.find(FIND_MY_CREEPS,
@@ -30,11 +20,22 @@ module.exports = {
         }
 
         // Emergency repair if a structure has less than 5% health
-        targets = tower.room.find(FIND_STRUCTURES,
+        let targets = tower.room.find(FIND_STRUCTURES,
             {filter: (s) => s.structureType != STRUCTURE_WALL &&
                             s.structureType != STRUCTURE_RAMPART &&
                             s.hits / s.hitsMax < 0.05});
         if (targets.length > 0) {
+            tower.repair(targets[0]);
+            return;
+        }
+
+        // Repair walls and ramparts until health === cb.repairWallsTo
+        targets = tower.room.find(FIND_STRUCTURES,
+            {filter: (s) => (s.structureType == STRUCTURE_WALL ||
+                            s.structureType == STRUCTURE_RAMPART) &&
+                            s.hits < cb.repairWallsTo});
+        targets = _.sortBy(targets, (t) => t.hits);
+        if (targets.length) {
             tower.repair(targets[0]);
         }
     }
