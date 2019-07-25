05f2b9b145556f2e10d897ed2f8c2140c43b6c42
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-07-25 13:53:59 -0700

    Reorder, refactor, and improved creep counting

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ var cb = require("controlBoard");
 module.exports.loop = function () {
     // --------------------------------------- TODO LIST --------------------------------------- //
     // TODO: Create better? flee move function for creep
-    // TODO: Have creeps store a path in memory
-    // TODO: Put a hash in every spawner keeping track of every creep it owns,
-    //          updates by iterating through Game.creeps
+    // TODO: Have creeps put a target into memory instead of finding it again every tick [HIGH PRIORITY]
+    //             DONE FOR HARVESTER, LOGISTIC,
+
+    let creepCount = {};
+    // --------------------------------------- ROOM AI --------------------------------------- //
+    for (const name in Game.rooms) {
+        room = Game.rooms[name];
+        if (room.storage != undefined)
+            room.visual.text(room.storage.store[RESOURCE_ENERGY], room.storage.pos);
+
+        creepCount[name] = {};
+        for (const role of cb.listOfRoles) {
+            creepCount[name][role] = 0;
+        }
+    }
 
     // --------------------------------------- SPAWN AI --------------------------------------- //
-    let creepCount = {};
     for (const name in Game.spawns) {
         //Game.spawns[name].update();
         Game.spawns[name].spawnIfNeeded();
@@ -19,23 +30,24 @@ module.exports.loop = function () {
 
         if (creepCount[Game.spawns[name].room.name] == undefined) {
             creepCount[Game.spawns[name].room.name] = {};
-            for (const role of cb.listOfRoles) {
-                creepCount[Game.spawns[name].room.name][role] = 0;
-            }
         }
+
     }
 
     // --------------------------------------- CREEP AI --------------------------------------- //
-    // TODO: Fix this method of couting creep's tendancy to give undefined for roles with 0
     for (const name in Game.creeps) {
         const creep = Game.creeps[name];
 
         // Create a javascipt object for counting creeps
         let roomEntry = creepCount[creep.memory.home][creep.memory.role];
-        if (roomEntry == undefined)
-            console.log("ERROR found a invalid role: " + creep.memory.role);
-        else
+        if (roomEntry != undefined)
             creepCount[creep.memory.home][creep.memory.role] += 1;
+        else
+            creepCount[creep.memory.home][creep.memory.role] = 0;
+
+//        if (creep.memory.role == 'healer') {
+//            creep.memory.target = "W34N2";
+//        }
 
         creep.doRole();
     }
@@ -48,23 +60,6 @@ module.exports.loop = function () {
     const towers = _.filter(Game.structures, (s) => s.structureType == STRUCTURE_TOWER);
     for (const tower of towers) {
         towerAI.run(tower);
-    }
-
-    // --------------------------------------- ROOM AI --------------------------------------- //
-    for (const name in Game.rooms) {
-        room = Game.rooms[name];
-        if (room.storage != undefined)
-            room.visual.text(room.storage.store[RESOURCE_ENERGY], room.storage.pos);
-
-            /*
-        const critials = room.find(FIND_STRUCTURES,
-            {filter: (s) => s.structureType != STRUCTURE_WALL &&
-                            s.structureType != STRUCTURE_RAMPART &&
-                            s.hits < s.hitsMax});
-        for (let critical of critials) {
-            critical.room.visual.text("X", critical.pos, {color:'#ff0000'});
-        }
-        */
     }
 
     // --------------------------------------- MEMORY CLEAR --------------------------------------- //
